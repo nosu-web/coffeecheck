69f1c4a0cfd809d331473485065084a9ae1d19ac
goudiev <t.goudiev@yandex.ru>
2021-05-12 08:30:30 +0300

    v2.0

---
+++ assets/js/map.js
@@ -0,0 +1,15 @@
+$( ".toggle-btn" ).click(function() {
+    $( ".toggle-btn" ).toggleClass( "toggle-btn-closed" );
+    $( ".sidebar" ).toggle();
+});
+
+var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'your.mapbox.access.token'
+}).addTo(mymap);
---
+++ templates/map.tpl.php
@@ -0,0 +1,9 @@
+<div class="row">
+    <div class="col p-0">
+        <div id="mapid"></div>
+        <button type="button" class="toggle-btn btn bg-milk position-absolute top-50 start-100 translate-middle">
+        <i class="bi bi-gear"></i>
+        </button>
+    </div>
+    <div class="sidebar bg-latte" id="sidebar-wrapper"></div>
+</div>
